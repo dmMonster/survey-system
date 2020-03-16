fdc1e2ad31569b93515e5c5e5e09d7dd851459a8
dmMonster <maciaszek5@interia.eu>
2020-03-16 10:05:35 +0100

    Add footer and brands icons

---
+++ resources/js/components/footer/Footer.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import { faTwitter, faFacebook, faGoogle } from '@fortawesome/free-brands-svg-icons'
+
+const Footer = () => {
+    return (
+        <footer className="row m-0 bg-dark text-white p-3">
+            <div className="col-12 text-center">
+                <a className="p-3" href="#">
+                    <FontAwesomeIcon icon={faTwitter}  color={"#f2f4f4"} size={"lg"}/>
+                </a>
+                <a className="p-3" href="#">
+                    <FontAwesomeIcon icon={faFacebook}  color={"#f2f4f4"} size={"lg"}/>
+                </a>
+                <a className="p-3" href="#">
+                    <FontAwesomeIcon icon={faGoogle}  color={"#f2f4f4"} size={"lg"}/>
+                </a>
+
+            </div>
+            <div className="col-12">
+                <h6 className="text-center">© Copyright {new Date().getFullYear()} Survio. All rights reserved.</h6>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
