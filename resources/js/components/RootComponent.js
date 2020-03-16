59fcb473a148c1b46b187383c888bb07bb7a0661
dmMonster <42850304+dmMonster@users.noreply.github.com>
2020-03-16 09:23:23 +0100

    Add fontawesome icons, prepare new features section (#18)
    Simple feature list.

--- resources/js/components/RootComponent.js
+++ resources/js/components/RootComponent.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from "./header/Header.jsx";
+import FeatureList from "./featureList/FeatureList";
 
 function RootComponent() {
     return (
         <div className="container-fluid">
             <Header />
+            <FeatureList/>
         </div>
     );
 }
