d7ce9e67ef61ecb325b49005b85e85f9922619b7
dmMonster <42850304+dmMonster@users.noreply.github.com>
2020-03-17 16:31:35 +0100

    Add footer and brands icons (#19)
    * Add footer and brands icons

--- resources/js/components/RootComponent.js
+++ resources/js/components/RootComponent.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from "./header/Header.jsx";
 import FeatureList from "./featureList/FeatureList";
+import Footer from "./footer/Footer";
 
 function RootComponent() {
     return (
         <div className="container-fluid">
             <Header />
             <FeatureList/>
+            <Footer/>
         </div>
     );
 }
