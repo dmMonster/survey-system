ab7cc80f4f9705e3f7989c119bed1b36deb9f178
dmMonster <maciaszek5@interia.eu>
2020-03-23 12:17:57 +0100

    Add: Admin can select specific user and edit.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
@@ -10,7 +13,49 @@ class UserController extends Controller
      * Get all users
      * @return mixed
      */
-    public function index() {
+    public function index()
+    {
         return User::get();
+    }
+
+
+    /**
+     * Display the specified resource.
+     *
+     * @param int $id
+     * @return Response
+     */
+    public function show($id)
+    {
+        return User::where('id', $id)->get();
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param Request $request
+     * @param int $id
+     * @return Response
+     */
+    public function update(Request $request, $id)
+    {
+
+        $validate = Validator::make($request->all(['name', 'email', 'password']), [
+            'name' =>['string', 'max:255'],
+            'email' =>['string', 'email', 'max:255'],
+
+        ]);
+
+        if($validate->fails()){
+            return response($validate->messages(),400);
+        }
+
+        return User::where("id", $id)->first()->update([
+            "name" => $request->input("name"),
+            "email" => $request->input("email"),
+            "is_admin" => boolval($request->input("is_admin")),
+        ]);
+
     }
 }
