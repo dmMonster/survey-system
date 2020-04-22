7d44c0a8a0381e68af097e864fe26728ced23101
dmMonster <maciaszek5@interia.eu>
2020-04-22 18:34:37 +0200

    Database design and models.

---
+++ app/GivenAnswer.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class GivenAnswer extends Model
+{
+    public function result()
+    {
+        return $this->belongsTo(Result::class);
+    }
+
+    public function question()
+    {
+        return $this->belongsTo(Question::class);
+    }
+
+    public function answer()
+    {
+        return $this->belongsTo(Answer::class);
+    }
+}
