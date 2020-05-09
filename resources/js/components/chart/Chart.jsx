6105af13710f112fce6ddbf43326954acc4cdfc0
dmMonster <maciaszek5@interia.eu>
2020-05-09 11:26:14 +0200

    add: chart - random colors

--- resources/js/components/chart/Chart.jsx
+++ resources/js/components/chart/Chart.jsx
@@ -3,86 +3,94 @@ import ChartJs from 'chart.js';
 import PropTypes from 'prop-types'
 
 const Chart = props => {
-    const canvas = useRef(null);
+        const canvas = useRef(null);
 
-    const [chartType, setChartType] = useState('bar');
-    useEffect(() => {
-        const chartOptions = {
-            legend: {
-                display: false,
-            },
-            pointBackgroundColor: '#ff493b',
-            tooltips: {
-                callbacks: {
-                    title: function (value) {
-                        const regex = /\b[\w']+(?:[^\w\n]+[\w']+){0,5}\b/g;
-                        return value[0]['xLabel'].match(regex);
+        const [chartType, setChartType] = useState('bar');
+        useEffect(() => {
+            const chartOptions = {
+                legend: {
+                    display: false,
+                },
+                pointBackgroundColor: '#ff493b',
+                tooltips: {
+                    callbacks: {
+                        title: function (value) {
+                            const regex = /\b[\w']+(?:[^\w\n]+[\w']+){0,5}\b/g;
+                            return value[0]['xLabel'].match(regex);
+                        }
                     }
                 }
-            }
-        };
+            };
 
-        if (chartType === 'bar') {
-            chartOptions['scales'] = {
-                xAxes: [{
-                    stacked: true,
+            if (chartType === 'bar') {
+                chartOptions['scales'] = {
+                    xAxes: [{
+                        stacked: true,
+                        ticks: {
+                            callback: function (value) {
+                                if (value.length > 20) {
+                                    return value.substr(0, 10) + '...';
+                                } else {
+                                    return value
+                                }
+
+                            },
+                        }
+                    }],
+                    yAxes: [{
+                        stacked: true,
+                    }]
+                }
+            } else if (chartType === 'radar') {
+                chartOptions['scale'] = {
                     ticks: {
-                        callback: function (value) {
-                            if (value.length > 20) {
-                                return value.substr(0, 20) + '...';
-                            } else {
-                                return value
-                            }
-
-                        },
+                        beginAtZero: true,
+                        min: 0,
                     }
-                }],
-                yAxes: [{
-                    stacked: true,
-                }]
-            }
-        } else if (chartType === 'radar') {
-            chartOptions['scale'] = {
-                ticks: {
-                    beginAtZero: true,
-                    min: 0,
                 }
             }
+
+            const ChartObject = new ChartJs(canvas.current.getContext('2d'), {
+                type: chartType,
+                data: {
+                    labels: props.labels,
+                    datasets: [{
+                        backgroundColor: randomColors(props.labels.length),
+                        data: props.dataset,
+                        borderColor: 'rgba(35,110,164,0.96)',
+                    }],
+                },
+                options: chartOptions,
+            });
+            return function cleanUp() {
+                ChartObject.destroy();
+            }
+        }, [chartType]);
+
+
+        function randomColors(amount) {
+            let colors = [];
+            for (let i = 0; i < amount; i++) {
+                colors.push('#' + Math.random().toString(16).substr(2, 6));
+            }
+            return colors;
+
         }
 
-        const ChartObject = new ChartJs(canvas.current.getContext('2d'), {
-            type: chartType,
-            data: {
-                labels: props.labels,
-                datasets: [{
-                    backgroundColor: ['rgb(255, 99, 132)', 'rgb(0,133,115)', 'rgb(41,106,163)'],
-                    data: props.dataset,
-                    borderColor: 'rgba(6,132,10,0.77)',
-                }],
-            },
-            options: chartOptions,
-        });
-        return function cleanUp() {
-            ChartObject.destroy();
-        }
-    }, [chartType]);
-
-    return (
-        <div>
-            <h2 className="text-center">{props.title}</h2>
+        return (
             <div>
+                <h2 className="text-center">{props.title}</h2>
                 <h5>Chart type</h5>
-                <button onClick={() => setChartType('bar')}>Bar</button>
-                <button onClick={() => setChartType('pie')}>Pie</button>
-                <button onClick={() => setChartType('radar')}>Radar</button>
+                <button className="btn btn-secondary" onClick={() => setChartType('bar')}>Bar</button>
+                <button className="btn btn-secondary" onClick={() => setChartType('pie')}>Pie</button>
+                <button className="btn btn-secondary" onClick={() => setChartType('radar')}>Radar</button>
+                
+                <canvas ref={canvas} height="150px">
+                </canvas>
             </div>
-            <canvas ref={canvas} height="150px">
-
-            </canvas>
-
-        </div>
-    );
-};
+        );
+    }
+;
 
 Chart.propTypes = {
     title: PropTypes.string.isRequired,
