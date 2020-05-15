03fe08058ba2e439a98bedd32aeb910b362f7ee0
dmMonster <42850304+dmMonster@users.noreply.github.com>
2020-05-15 19:34:04 +0200

    Fix: admin cannot delete last admin account. (#99)

--- resources/js/components/deleteUser/DeleteUser.jsx
+++ resources/js/components/deleteUser/DeleteUser.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {useParams, useHistory} from 'react-router-dom';
 import {userService} from "../../_services/userService";
 
@@ -12,15 +12,23 @@ const DeleteUser = () => {
         history.goBack();
     };
 
+    const [error, setError] = useState(null);
     const deleteUser = () => {
         userService.deleteUser(id).subscribe({
             next() {history.goBack()},
-            error(error){console.log(error.response.data)}
+            error(error){
+                setError(error.response.data.message);
+            }
         })
     };
 
     return (
         <div className="container">
+
+            <div className={(!error ? 'd-none' : null) + ' alert alert-danger'}>
+                {error}
+            </div>
+
             <div>
                 <h4 className="text-center">Are you sure?</h4>
                 <div className="d-flex justify-content-center">
