63da141bf885c988d4eab2d1b8c3f2f42ca9249b
dmMonster <maciaszek5@interia.eu>
2020-06-04 17:58:50 +0200

    heroku: dev version

--- app/Policies/SurveyRatingPolicy.php
+++ app/Policies/SurveyRatingPolicy.php
@@ -9,6 +9,7 @@ class SurveyRatingPolicy
 {
     public function viewAny(User $user, int $surveyId): bool
     {
+        dd(Survey::where('id', $surveyId)->first()['id']);
         return Survey::where('id', $surveyId)->first()['id'] === $user['id'] || $user->checkAdmin();
     }
 
